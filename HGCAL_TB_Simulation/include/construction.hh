83ce66e514ac1873805e7684b8c348bb9e3cfb60
PraveshSharma1729 <149511820+PraveshSharma1729@users.noreply.github.com>
2024-11-26 14:16:14 +0530

    Add files via upload

---
+++ HGCAL_TB_Simulation/include/construction.hh
@@ -0,0 +1,36 @@
+#ifndef CONSTRUCTION_HH
+#define CONSTRUCTION_HH
+
+#include "G4VUserDetectorConstruction.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4VPhysicalVolume.hh"
+#include "G4Box.hh"
+#include "G4LogicalVolume.hh"
+#include "G4NistManager.hh"
+#include "G4PVPlacement.hh"
+#include "detector.hh"
+#include "G4Polyhedra.hh"
+
+#include "G4VisAttributes.hh"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include "G4ThreeVector.hh" 
+#include "G4RotationMatrix.hh"
+
+class MyDetectorConstruction : public G4VUserDetectorConstruction
+{
+public:
+MyDetectorConstruction();
+~MyDetectorConstruction();
+
+virtual G4VPhysicalVolume *Construct();
+
+private:
+G4LogicalVolume *logicDetector;
+virtual void ConstructSDandField();
+
+};
+#endif
